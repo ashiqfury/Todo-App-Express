06eb8c135c40104c79b4124745fd213a8ec63786
Fury <ashiqasraf07@gmail.com>
2021-07-14 19:54:05 +0530

    environment variable file added

--- index.js
+++ index.js
@@ -1,35 +1,37 @@
-const express = require('express')
-const mongoose = require('mongoose')
+require('dotenv').config();
 
-const app = express()
-const hostname = 'localhost'
-const port = process.env.PORT || 2506
+const express = require('express');
+const mongoose = require('mongoose');
 
-const todoRouter = require('./routes/todoRoutes')
-const Todo = require('./models/todoModel')
+const app = express();
+const hostname = 'localhost';
+const port = process.env.PORT || 2506;
+
+const todoRouter = require('./routes/todoRoutes');
+const Todo = require('./models/todoModel');
 
 // Setting 'public' as static folder
-app.use(express.static('public'))
+app.use(express.static('public'));
 
 // Post from parser
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: true }));
 
 // Setting ejs template engine
-app.set('view engine', 'ejs')
+app.set('view engine', 'ejs');
 
 // Mongo db connection
-const dbUser = 'fury'
-const dbPassword = 'test1234'
-const dbName = 'node-tuts'
-const dbURI = `mongodb+srv://${dbUser}:${dbPassword}@cluster0.mfyow.mongodb.net/${dbName}?retryWrites=true&w=majority`
+const dbUser = process.env.DB_HOST_NAME;
+const dbPassword = process.env.DB_SECRET;
+const dbName = process.env.DB_NAME;
+const dbURI = `mongodb+srv://${dbUser}:${dbPassword}@cluster0.mfyow.mongodb.net/${dbName}?retryWrites=true&w=majority`;
 mongoose
 	.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
 	.then((response) => console.log('Connected to database!'))
-	.catch((err) => console.log('Connection Failed!', err))
+	.catch((err) => console.log('Connection Failed!', err));
 
 app.get('/', (req, res) => {
-	res.redirect('/todo')
-})
+	res.redirect('/todo');
+});
 
 // Post req
 
@@ -37,23 +39,23 @@ app.get('/', (req, res) => {
 app.get('/todo/get-all', (req, res) => {
 	Todo.find()
 		.then((result) => {
-			res.send(result)
+			res.send(result);
 		})
 
-		.catch((err) => console.log(err))
-})
+		.catch((err) => console.log(err));
+});
 
 // All todo routes
-app.use(todoRouter)
+app.use(todoRouter);
 
 // About page
 app.get('/about', (req, res) => {
-	res.render('about', { title: 'About Page' })
-})
+	res.render('about', { title: 'About Page' });
+});
 
 // 404 page
 app.use((req, res) => {
-	res.status(404).render('404', { title: '404 Page' })
-})
+	res.status(404).render('404', { title: '404 Page' });
+});
 
-app.listen(port, () => console.log(`Server run on http://${hostname}:${port}`))
+app.listen(port, () => console.log(`Server run on http://${hostname}:${port}`));
